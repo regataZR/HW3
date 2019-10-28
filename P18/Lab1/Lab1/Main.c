cb01ea25d4803330972a4988e529733d9bac9f13
regataZR <regata777711@gmail.com>
2019-10-29 01:14:29 +0800

    upload

--- P18/Lab1/Lab1/Main.c
+++ P18/Lab1/Lab1/Main.c
@@ -5,7 +5,7 @@ void main(void) {
 	char string1[60] = "Welcome";
 	char string2[60];
 	int len;
-	strcpy_s(string2, string1);
+	strcpy_s(string2, _countof(string1),string1);
 	printf("string2=%s\n", string2);
 	len = strlen(string2);
 	printf("¦r¦êªø«×¬°%d\n", len);
